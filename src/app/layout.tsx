3a6700ae58a2ed6794109bc849621496f3da2554
airRnot1106 <yngw.rintaro@gmail.com>
2023-02-09 16:22:41 +0900

    SiteHeaderを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import { RecoilRoot } from 'recoil';
 
 import '@/app/globals.css';
+import { SiteHeader } from '@/components/domain/site/molecules/SiteHeader';
 import { DebugObserver } from '@/components/functional/DebugObserver';
 
 import { I18nProvider } from '@/providers/I18nProvider';
@@ -24,7 +25,9 @@ export default function RootLayout({
           <DebugObserver />
           <I18nProvider>
             <>
-              <div className="fixed top-0 left-0 z-50 h-[10dvh] w-full bg-base-200"></div>
+              <div className="fixed top-0 left-0 z-50 h-[10dvh] w-full bg-base-200">
+                <SiteHeader />
+              </div>
               <div className="z-0 h-[83dvh] bg-base-100">
                 <div className="h-[10dvh]"></div>
                 {children}
