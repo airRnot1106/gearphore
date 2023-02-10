f111c4af533ded304e6782c06b2c9ea70823874d
airRnot1106 <yngw.rintaro@gmail.com>
2023-02-10 16:07:36 +0900

    CssSizeを追加

--- src/types/index.ts
+++ src/types/index.ts
@@ -55,6 +55,51 @@ export type Alert = z.infer<typeof alertSchema>;
 export const shapeSchema = z.enum(['square', 'circle']).optional();
 export type Shape = z.infer<typeof shapeSchema>;
 
+const cssLSDSchema = z.union([
+  z.literal('l'),
+  z.literal('s'),
+  z.literal('d'),
+  z.literal(''),
+]);
+type CssLSD = z.infer<typeof cssLSDSchema>;
+
+export const cssCommonUnitSchema = z.union([
+  z.literal('px'),
+  z.literal('em'),
+  z.literal('ex'),
+  z.literal('ch'),
+  z.literal('rem'),
+  z.literal('lh'),
+  z.custom<`${CssLSD}vmin`>((val) => /^[lsd]?vmin$/.test(val as string)),
+  z.custom<`${CssLSD}vmax`>((val) => /^[lsd]?vmax$/.test(val as string)),
+  z.literal('%'),
+]);
+export type CssCommonUnit = z.infer<typeof cssCommonUnitSchema>;
+
+export const cssWidthUnit = z.union([
+  cssCommonUnitSchema,
+  z.custom<`${CssLSD}vw`>((val) => /^[lsd]?vw$/.test(val as string)),
+]);
+export type CssWidthUnit = z.infer<typeof cssWidthUnit>;
+
+export const cssHeightUnit = z.union([
+  cssCommonUnitSchema,
+  z.custom<`${CssLSD}vh`>((val) => /^[lsd]?vh$/.test(val as string)),
+]);
+export type CssHeightUnit = z.infer<typeof cssHeightUnit>;
+
+export const cssSizeUnit = z.union([cssWidthUnit, cssHeightUnit]);
+export type CssSizeUnit = z.infer<typeof cssSizeUnit>;
+
+export const cssWidth = z.tuple([z.number(), cssWidthUnit]);
+export type CssWidth = z.infer<typeof cssWidth>;
+
+export const cssHeight = z.tuple([z.number(), cssHeightUnit]);
+export type CssHeight = z.infer<typeof cssHeight>;
+
+export const cssSize = z.tuple([z.number(), cssSizeUnit]);
+export type CssSize = z.infer<typeof cssSize>;
+
 /* Domain */
 
 export const localSchema = z.enum(['ja', 'en']);
