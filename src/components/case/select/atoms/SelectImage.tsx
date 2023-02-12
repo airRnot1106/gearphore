a73ce6b1a11a139f34eccb24f8b01f15fe34c96f
airRnot1106 <yngw.rintaro@gmail.com>
2023-02-12 13:09:37 +0900

    power-iconを追加

---
+++ src/components/case/select/atoms/SelectImage.tsx
@@ -0,0 +1,29 @@
+import type { LabelValue } from '@/types';
+
+export type SelectImageProps = {
+  current: React.ReactNode;
+  options: LabelValue[];
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+export const SelectImage = ({
+  current,
+  options,
+  onChange,
+}: SelectImageProps) => {
+  return (
+    <div className="relative inline-block">
+      {current}
+      <select
+        className="absolute top-0 left-0 h-full w-full opacity-0"
+        onChange={onChange}
+      >
+        {options.map(({ label, value }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
