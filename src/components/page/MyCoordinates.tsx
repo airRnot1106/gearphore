62af08efc52c72f1e198743cca3dcf292940b788
airRnot1106 <yngw.rintaro@gmail.com>
2023-02-12 21:42:05 +0900

    CoordinateListを追加

--- src/components/page/MyCoordinates.tsx
+++ src/components/page/MyCoordinates.tsx
@@ -1,8 +1,10 @@
 import { CoordinateAddButton } from '@/components/domain/coordinate/atoms/CoordinateAddButton';
+import { CoordinateList } from '@/components/domain/coordinate/molecules/CoordinateList';
 
 export const MyCoordinates = () => {
   return (
     <div>
+      <CoordinateList />
       <div className="fixed bottom-20 right-8">
         <CoordinateAddButton />
       </div>
