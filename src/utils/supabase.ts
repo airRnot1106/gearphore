c736c4a7d51793facf8c655874d3aa7bc293a4f4
airRnot1106 <yngw.rintaro@gmail.com>
2023-02-13 17:58:02 +0900

    仮で追加

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import { z } from 'zod';
 
 export const supabase = createClient(
   process.env['NEXT_PUBLIC_SUPABASE_URL'] as string,
@@ -18,4 +19,29 @@ export const signInWithGoogle = async () => {
 export const signOut = async () => {
   const { error } = await supabase.auth.signOut();
   return { error };
+};
+
+export const coordinateDatabaseSchema = z.object({
+  id: z.string().uuid(),
+  coordinate_json: z.string(),
+  created_at: z.date(),
+});
+export type CoordinateDatabase = z.infer<typeof coordinateDatabaseSchema>;
+
+export const existsUser = async (id: string) => {
+  const { data, error } = await supabase
+    .from('coordinates')
+    .select('id')
+    .eq('id', id)
+    .single();
+  return { data, error };
+};
+
+export const insertUser = async (id: string) => {
+  console.log(await supabase.auth.getSession());
+  const { data, error } = await supabase
+    .from('coordinates')
+    .insert({ id, coordinate_json: '[]' })
+    .single();
+  return { data, error };
 };
