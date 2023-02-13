4c47226214f290e063ae6ef8b93cca0a4e329b63
airRnot1106 <yngw.rintaro@gmail.com>
2023-02-13 15:41:04 +0900

    consoleを削除

--- src/components/functional/Authenticator.tsx
+++ src/components/functional/Authenticator.tsx
@@ -14,17 +14,14 @@ export const Authenticator = () => {
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data }) => {
-      console.log(data);
       if (data) {
         initializeUser({ session: data.session });
       }
     });
     supabase.auth.onAuthStateChange((event, session) => {
       if (event === 'SIGNED_IN') {
-        console.log('SIGNED_IN', session);
         initializeUser({ session });
       } else if (event === 'SIGNED_OUT') {
-        console.log('SIGN_OUT');
         resetUser();
         resetCoordinates();
       }
